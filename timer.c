8285f913e7f64cb1201b6becbb390d062c4158d0
Parikshit <parikshit15155@iiitd.ac.in>
2016-09-09 19:28:31 +0530

    Timer C file added

--- timer.c
+++ timer.c
@@ -2,14 +2,14 @@
 #include <avr/interrupt.h>
 int main()
 {
-	PRR |= (unsigned char)((1<<PRADC)|(1<<PRTIM0)|(1<<PRTIM2)|(1<<PRSPI)|(1<<PRTWI)|(0<<PRUSART0));
-	DDRB |= 1 << 5;
-	TCCR1B = 1 << WGM12;
-	OCR1A = 7812.5;
+	PRR |= (unsigned char)((1 << PRSPI)|(1 << PRTIM2)|(1 << PRTIM0)|(1 << PRADC)|(1 << PRTWI));	 // Setting all the other registers off
+	DDRB |= 1 << 5;	//Setting fifth pin as output
+	TCCR1B = 1 << WGM12;	
+	OCR1A = 7812.5; // Setting timer ticks
 	TIMSK1 = 1 << OCIE1A;
-	sei();
+	sei();	//Enable global interrupt
 	TCCR1B |= (1 << CS12) | (1 << CS10);
-	PORTB ^= 1 << 5; 
+	PORTB ^= 1 << 5; //toggle the led
 	while(1)
 	{
 
